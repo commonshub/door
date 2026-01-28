c26ed7c6711383ed5b2e69b62e86aede3b3cc1b1
Xavier Damman <xdamman@gmail.com>
2026-01-28 18:57:49 +0100

    update

--- server/lib/utils.js
+++ server/lib/utils.js
@@ -1,6 +1,13 @@
 import fs from "fs";
 import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 export const loadJSON = (filePath) => {
-  filePath = path.join(process.cwd(), filePath);
+  // Resolve relative to the server directory (parent of lib/)
+  const serverDir = path.join(__dirname, "..");
+  filePath = path.join(serverDir, filePath);
   return JSON.parse(fs.readFileSync(filePath, "utf8"));
 };
